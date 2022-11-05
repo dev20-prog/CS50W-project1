dde165b1a167509d1744a7b92978f8f91f41b88e
Aryan Rai <tashiyin128@gmail.com>
2022-11-05 15:39:56 +0530

    New Entry page added

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django import forms
+from django.core.files.storage import default_storage
 from . import util
 
 
@@ -47,11 +48,42 @@ def search(request):
 
 
 class NewEntry(forms.Form):
-    title = forms.CharField(label="Title:")
-    content = forms.CharField(widget=forms.Textarea)
+    title = forms.CharField(label="Title")
+    content = forms.CharField(label="Content", widget=forms.Textarea)
 
 
 def newpage(request):
+
+    if request.method =="POST":
+        
+        message = ""
+
+        form = NewEntry(request.POST)
+
+        if form.is_valid():
+
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            filename = f"entries/{title}.md"
+
+            if default_storage.exists(filename):
+                message = "Error: An entry by this title already exists."
+                return render(request, "encyclopedia/newpage.html", {
+                    "form": form,
+                    "message": message
+                })
+
+            util.save_entry(title, content)
+            entry = util.get_entry(title)
+            return render(request, "encyclopedia/entry.html", {
+                "title": title,
+                "entry": entry
+            })
+
+        return render(request, "encyclopedia/newpage.html", {
+            "form": form
+        })
+        
     return render(request, "encyclopedia/newpage.html", {
         "form": NewEntry()
     })
