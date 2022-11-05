516b012d76efbd41e282b2db0b8c9bf1a3f8cc68
Aryan Rai <tashiyin128@gmail.com>
2022-11-05 11:48:53 +0530

    Third commit

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.entry, name="entry")
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("search", views.search, name="search"),
+    path("newpage", views.newpage, name="newpage")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -10,26 +10,49 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
 def entry(request, title):
-    if request.method == "POST":
-        query = request.POST.get('q')
-        entry = util.get_entry(query)
-        entries = util.list_entries()
-        if entry == None:
-            results = []
-            for i in entries:
-                if query.lower() in i.lower():
-                    results.append(i)
-            return render(request, "encyclopedia/search.html", {
-                "title": "Search Results",
-                "entry": results
-            })
-        return render(request, "encyclopedia/entry.html", {
-            "title": query,
-            "entry": entry
-        })
+
     entry = util.get_entry(title)
+    if entry == None:
+        return render(request, "encyclopedia/entry.html", {
+            "title": "Error",
+            "entry": "Error 404: Requested page not found."
+        })
     return render(request, "encyclopedia/entry.html", {
-        "entry": entry,
-        "title": title.capitalize()
+        "title": title.capitalize(),
+        "entry": entry
     })
+
+def search(request):
+    
+    query = request.POST.get('q')
+    entry = util.get_entry(query)
+    entries = util.list_entries()
+
+    if entry == None:
+        results = []
+        for i in entries:
+            if query.lower() in i.lower():
+                results.append(i)
+        return render(request, "encyclopedia/search.html", {
+            "title": "Search Results",
+            "results": results
+        })
+
+    return render(request, "encyclopedia/entry.html", {
+        "title": query.capitalize(),
+        "entry": entry
+    })
+
+
+class NewEntry(forms.Form):
+    title = forms.CharField(label="Title:")
+    content = forms.CharField(widget=forms.Textarea)
+
+
+def newpage(request):
+    return render(request, "encyclopedia/newpage.html", {
+        "form": NewEntry()
+    })
+        
